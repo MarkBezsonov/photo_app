84e4992a63568506dc9085c9940b814d07b04fd6
MarkBezsonov <markbezsonov@gmail.com>
2021-11-17 20:46:35 -0600

    Add image size validations

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,13 @@
 class Image < ApplicationRecord
   belongs_to :user
   mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "The maximum photo size is 5MB per photo.")
+    end
+  end
 end
